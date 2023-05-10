04cebbe83344edb31e0e89ba9a50499fb90cd612
Thitiwut Somsa <tsomsa@student.42bangkok.com>
2023-05-11 00:19:56 +0700

    start web

---
+++ js/render.js
@@ -0,0 +1,15 @@
+const components = [
+	'header',
+	'schedule',
+];
+const path = './components';
+for (const name of components) {
+	fetch(`${path}/${name}.html`)
+		.then(response => response.text())
+		.then(data => {
+			const doc = document.querySelector(name);
+			if (doc) {
+				doc.innerHTML = data;
+			}
+		}); 
+}
